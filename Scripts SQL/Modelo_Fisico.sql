58644a6352ed15076fc83d68dd9bb7ad9135d043
Oli <oliverlopez42069@gmail.com>
2024-07-09 00:11:33 -0500

    feat: :hammer: Modelo físico

---
+++ Scripts SQL/Modelo_Fisico.sql
@@ -0,0 +1,154 @@
+-- ##########################################################
+-- ### PROYECTO BBDD - Creación de tablas (Modelo físico) ###
+-- ##########################################################
+
+CREATE DATABASE IF NOT EXISTS Ambientales;
+
+USE Ambientales;
+
+CREATE TABLE Departamento (
+    ID_Departamento INT NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE Parque (
+    ID_Parque INT NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL,
+    Fecha_Declaracion DATE NOT NULL,
+    Superficie_Total DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE Departamento_Parque (
+    ID_Departamento INT NOT NULL,
+    ID_Parque INT NOT NULL,
+    PRIMARY KEY (ID_Departamento, ID_Parque),
+    FOREIGN KEY (ID_Departamento) REFERENCES Departamento(ID_Departamento),
+    FOREIGN KEY (ID_Parque) REFERENCES Parque(ID_Parque)
+);
+
+CREATE TABLE Entidad_Responsable (
+    ID_Entidad INT NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL,
+    ID_Departamento INT NOT NULL,
+    FOREIGN KEY (ID_Departamento) REFERENCES Departamento(ID_Departamento)
+);
+
+CREATE TABLE Area (
+    ID_Area INT NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL,
+    Extension DECIMAL(10,2) NOT NULL,
+    ID_Parque INT NOT NULL,
+    FOREIGN KEY (ID_Parque) REFERENCES Parque(ID_Parque)
+);
+
+CREATE TABLE Especie (
+    ID_Especie INT NOT NULL PRIMARY KEY,
+    Denominacion_Cientifica VARCHAR(255) NOT NULL,
+    Denominacion_Vulgar VARCHAR(255) NOT NULL,
+    Tipo ENUM('Vegetal', 'Animal', 'Mineral') NOT NULL
+);
+
+CREATE TABLE Especie_Area (
+    ID_Especie INT NOT NULL,
+    ID_Area INT NOT NULL,
+    Numero_Individuos INT NOT NULL,
+    PRIMARY KEY (ID_Especie, ID_Area),
+    FOREIGN KEY (ID_Especie) REFERENCES Especie(ID_Especie),
+    FOREIGN KEY (ID_Area) REFERENCES Area(ID_Area)
+);
+
+CREATE TABLE Alojamiento (
+    ID_Alojamiento INT NOT NULL PRIMARY KEY,
+    Capacidad INT NOT NULL,
+    Categoria VARCHAR(60) NOT NULL,
+    ID_Parque INT NOT NULL,
+    FOREIGN KEY (ID_Parque) REFERENCES Parque(ID_Parque)
+);
+
+CREATE TABLE Visitante (
+    ID_Visitante INT NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL,
+    Direccion VARCHAR(255) NOT NULL,
+    Profesion VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Visitante_Alojamiento (
+    ID_Visitante INT NOT NULL,
+    ID_Alojamiento INT NOT NULL,
+    PRIMARY KEY (ID_Visitante, ID_Alojamiento),
+    FOREIGN KEY (ID_Visitante) REFERENCES Visitante(ID_Visitante),
+    FOREIGN KEY (ID_Alojamiento) REFERENCES Alojamiento(ID_Alojamiento)
+);
+
+CREATE TABLE Personal (
+    ID_Personal INT NOT NULL PRIMARY KEY,
+    Numero_Cedula VARCHAR(20) NOT NULL,
+    Nombre VARCHAR(255) NOT NULL,
+    Direccion VARCHAR(255) NOT NULL,
+    Telefonos VARCHAR(255) NOT NULL,
+    Movil VARCHAR(20) NOT NULL,
+    Sueldo DECIMAL(10,2) NOT NULL,
+    Tipo_Personal ENUM('001', '002', '003', '004') NOT NULL,
+    ID_Parque INT NOT NULL,
+    FOREIGN KEY (ID_Parque) REFERENCES Parque(ID_Parque)
+);
+
+CREATE TABLE Personal_Gestion (
+    ID_Personal INT NOT NULL PRIMARY KEY,
+    ID_Entrada INT NOT NULL,
+    FOREIGN KEY (ID_Personal) REFERENCES Personal(ID_Personal)
+);
+
+CREATE TABLE Entrada (
+	ID_Entrada INT NOT NULL PRIMARY KEY,
+	ID_Parque INT,
+	FOREIGN KEY (ID_Parque) REFERENCES Parque(ID_Parque)
+);
+
+CREATE TABLE Vehiculo (
+	ID_Vehiculo INT NOT NULL PRIMARY KEY,
+	Tipo_Vehiculo VARCHAR(50),
+	Marca_Vehiculo VARCHAR(50)
+);
+
+CREATE TABLE Personal_Vigilancia (
+    ID_Personal INT NOT NULL PRIMARY KEY,
+    ID_Area INT NOT NULL,
+    ID_Vehiculo INT NOT NULL,
+    FOREIGN KEY (ID_Personal) REFERENCES Personal(ID_Personal),
+    FOREIGN KEY (ID_Area) REFERENCES Area(ID_Area),
+    FOREIGN KEY (ID_Vehiculo) REFERENCES Vehiculo(ID_Vehiculo)
+);
+
+CREATE TABLE Personal_Conservacion (
+    ID_Personal INT NOT NULL PRIMARY KEY,
+    ID_Area INT NOT NULL,
+    Especialidad VARCHAR(50),
+    FOREIGN KEY (ID_Personal) REFERENCES Personal(ID_Personal),
+    FOREIGN KEY (ID_Area) REFERENCES Area(ID_Area)
+);
+
+CREATE TABLE Proyecto_Investigacion (
+    ID_Proyecto INT NOT NULL PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Personal_Investigador (
+    ID_Personal INT NOT NULL PRIMARY KEY,
+    Titulacion VARCHAR(255),
+    FOREIGN KEY (ID_Personal) REFERENCES Personal(ID_Personal)
+);
+
+CREATE TABLE Investigador_Proyecto (
+    ID_Personal INT NOT NULL,
+    ID_Proyecto INT NOT NULL,
+    ID_Especie INT NOT NULL,
+    PRIMARY KEY (ID_Personal, ID_Proyecto, ID_Especie),
+    FOREIGN KEY (ID_Personal) REFERENCES Personal(ID_Personal),
+    FOREIGN KEY (ID_Proyecto) REFERENCES Proyecto_Investigacion(ID_Proyecto),
+    FOREIGN KEY (ID_Especie) REFERENCES Especie(ID_Especie)
+);
+
+-- Desarrollado por:
+-- Juan rivero / T.I 1096701639
+-- Oliver López / T.I 1097911715
